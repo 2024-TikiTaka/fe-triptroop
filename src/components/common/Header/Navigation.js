5641ee3a79daaf02240b18d5f4d3bf76808bb99e
ddalla0425 <154950177+ddalla0425@users.noreply.github.com>
2024-05-23 15:51:12 +0900

    #1 project settings (#2)
    * chore: update README.md
    
    * feat: 기본 레이아웃 구조 작성

--- src/components/common/Header/Navigation.js
+++ src/components/common/Header/Navigation.js
@@ -1,24 +1,33 @@
 import {NavLink} from "react-router-dom";
+import {Nav, Navbar} from "react-bootstrap";
+
+const CustomNavLink = ({ to, children }) => (
+    <NavLink to={to} className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+        {children}
+    </NavLink>
+);
 
 function Navigation() {
     return (
-        <div className="nav">
-            <img src="./logo.svg" alt="logo" />
-            <ul className="">
-                <li>
-                    <NavLink to="/">여행지 소개</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/travels">일정</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/travels">동행글</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/companions">문의하기</NavLink>
-                </li>
-            </ul>
-        </div>
+        <Navbar bg="" expand="lg">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link as={CustomNavLink} to="/travel">
+                        여행지 소개
+                    </Nav.Link>
+                    <Nav.Link as={CustomNavLink} to="/calender">
+                        일정
+                    </Nav.Link>
+                    <Nav.Link as={CustomNavLink} to="/companions">
+                        동행글
+                    </Nav.Link>
+                    <Nav.Link as={CustomNavLink} to="/inquiries">
+                        문의하기
+                    </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
