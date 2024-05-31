674c8c37f664def0f830fb4dbb32d513e9a7a29b
chk2023 <jidanmian777@gmail.com>
2024-05-31 17:22:47 +0900

    feat: 친구 목록 추가

--- src/components/chat/FriendList.js
+++ src/components/chat/FriendList.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const FriendList = () => {
+
+    const startPrivateChat = () => {
+        /* 채팅방 db 저장 */
+        
+        /* 채팅 목록으로 리다이렉트 */
+    };
+
     return (
         <div>
-            친구 목록
-        </div>      
+            <h1>친구 목록</h1>
+            <button onClick={startPrivateChat}>1:1 채팅</button>
+        </div>
+
     );
 };
 
