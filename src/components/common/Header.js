13a1190a46b95d2670fa31d984e153d3a7be14cb
may54ther <aha9654@gmail.com>
2024-06-07 10:38:16 +0900

    refactor: Header, Footer 변경

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,112 +1,142 @@
-import React, {useState} from "react";
-import ChatBox from "../chat/ChatBox";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+import { Button, Container, Image, Nav, Navbar, Offcanvas, } from "react-bootstrap";
+
 import { isLogin } from "../../utils/TokenUtils";
-import { NavLink } from "react-router-dom";
-import { Button, Col, Container, Image, Nav, Navbar, Offcanvas } from "react-bootstrap";
-import { LockFill, PersonPlusFill, UnlockFill } from "react-bootstrap-icons";
+
+import ChatBox from "../box/ChatBox";
+import ProfileBox from "../box/ProfileBox";
+import { callLogoutAPI } from "../../apis/UserAPICalls";
+
 
 const CustomNavLink = ({ to, children }) => (
-    <NavLink to={to} className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
+    <NavLink
+        to={to}
+        className={({ isActive }) =>
+            isActive ? "nav-link px-2 fs-5 active" : "nav-link px-2 fs-5"
+        }
+    >
         {children}
     </NavLink>
 );
 
 function Header() {
-    const [showChat, setShowChat] = useState(false);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const [ showChat, setShowChat ] = useState(false);
     const handleChatShow = () => setShowChat(true);
     const handleChatClose = () => setShowChat(false);
 
     function BeforeLogin() {
         return (
-            <div className="user-btns">
-                <Button className="" onClick={handleChatShow}>
-                    <span className="">채팅</span>
-                </Button>
-                <Button className="outline blue-900">
-                    <span className="d-none d-lg-inline-block">회원가입</span>
-                    <span className="d-lg-none">
-                        <PersonPlusFill className="blue-900" />
-                    </span>
-                </Button>
-                <Button className="blue-900">
-                    <span className="d-none d-lg-inline-block white">로그인</span>
-                    <span className="d-lg-none">
-                        <UnlockFill className="white" />
-                    </span>
-                </Button>
-            </div>
+            <>
+                <Offcanvas.Body>
+                    <Navbar.Collapse className="justify-content-between">
+                        <Nav className="justify-content-center">
+                            <Nav.Link as={CustomNavLink} to="/travel">
+                                여행지 소개
+                            </Nav.Link>
+                            <Nav.Link as={CustomNavLink} to="/schedule">
+                                일정
+                            </Nav.Link>
+                            <Nav.Link as={CustomNavLink} to="/companion">
+                                동행글
+                            </Nav.Link>
+                            <Nav.Link as={CustomNavLink} to="/inquiry">
+                                문의하기
+                            </Nav.Link>
+                        </Nav>
+                        <Nav className="text-end">
+                            <Button
+                                className="outline blue-900 fs-6 mx-1"
+                                onClick={() => navigate(`/login`)}
+                            >
+                                로그인
+                            </Button>
+                            <Button
+                                className="blue-900 white fs-6 mx-1"
+                                onClick={() => navigate(`/signup`)}
+                            >
+                                회원가입
+                            </Button>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Offcanvas.Body>
+            </>
         );
     }
 
-
     function AfterLogin() {
         return (
-            <Col className="user-btns">
-                <div className="user-btns">
-                    <Button className="outline blue-900">
-                        <span className="d-none d-lg-inline-block">마이페이지</span>
-                        <span className="d-lg-none">
-                            <Image src="/images/header_icon_mypage_basic.png" />
-                        </span>
-                    </Button>
-                    <Button className="blue-900">
-                        <span className="d-none d-lg-inline-block white">로그아웃</span>
-                        <span className="d-lg-none">
-                            <LockFill className="white" />
-                        </span>
-                    </Button>
-                </div>
-            </Col>
+            <>
+                <Offcanvas.Header>
+                    <Offcanvas.Title>
+                        <ProfileBox />
+                    </Offcanvas.Title>
+                </Offcanvas.Header>
+
+                <Offcanvas.Body>
+                    <Navbar.Collapse className="justify-content-between">
+                        <Nav className="justify-content-center">
+                            <Nav.Link as={CustomNavLink} to="/travel">
+                                여행지 소개
+                            </Nav.Link>
+                            <Nav.Link as={CustomNavLink} to="/schedule">
+                                일정
+                            </Nav.Link>
+                            <Nav.Link as={CustomNavLink} to="/companions">
+                                동행글
+                            </Nav.Link>
+                            <Nav.Link as={CustomNavLink} to="/inquiry">
+                                문의하기
+                            </Nav.Link>
+                        </Nav>
+                        <Nav className="text-end">
+                            <Button onClick={handleChatShow}>
+                                <span>채팅</span>
+                            </Button>
+                            <Button
+                                className="outline blue-900 fs-6 mx-1"
+                                onClick={() => navigate(`/mypage`)}
+                            >
+                                마이페이지
+                            </Button>
+                            <Button
+                                className="blue-900 whit fs-6 mx-1"
+                                onClick={() => dispatch(callLogoutAPI(), navigate(`/`))}
+                            >
+                                로그아웃
+                            </Button>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Offcanvas.Body>
+            </>
         );
     }
 
     return (
         <header className="header">
-            <Navbar expand="lg">
-                <Container fluid>
-                    <Navbar.Brand href="/#" className="logo-btn">
-                        <Image src="/images/logo.svg" fluid/>
+            <Navbar expand="md py-3">
+                <Container fluid="lg">
+                    <Navbar.Brand
+                        className="logo-btn col-md-3"
+                        onClick={() => navigate(`/`)}
+                    >
+                        <Image src="/images/logo.svg" fluid />
                     </Navbar.Brand>
 
-                    <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg"/>
-                    <Navbar.Offcanvas
-                        id="offcanvasNavbar-expand-lg"
-                        aria-labelledby="offcanvasNavbarLabel-expand-lg"
-                        placement="end"
-                    >
-                        <Offcanvas.Header closeButton>
-                            <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">
-                                제목
-                            </Offcanvas.Title>
-                        </Offcanvas.Header>
-                        <Offcanvas.Body>
-                            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-between">
-                                <Nav>
-                                    {/* <Nav className="justify-content-end flex-grow-1 pe-3"> */}
-                                    <Nav.Link as={CustomNavLink} to="/travel">
-                                        여행지 소개
-                                    </Nav.Link>
-                                    <Nav.Link as={CustomNavLink} to="/schedule">
-                                        일정
-                                    </Nav.Link>
-                                    <Nav.Link as={CustomNavLink} to="/companions">
-                                        동행글
-                                    </Nav.Link>
-                                    <Nav.Link as={CustomNavLink} to="/inquiry">
-                                        문의하기
-                                    </Nav.Link>
-                                </Nav>
-
-                                <Nav>
-                                    {isLogin() ? <AfterLogin/> : <BeforeLogin/>}
-                                </Nav>
-                            </Navbar.Collapse>
-                        </Offcanvas.Body>
+                    <Navbar.Toggle />
+                    <Navbar.Offcanvas placement="end">
+                        {isLogin() ? <AfterLogin /> : <BeforeLogin />}
                     </Navbar.Offcanvas>
                 </Container>
             </Navbar>
+
+            {/* 채팅 */}
             <div className="chat-container">
-                <ChatBox show={showChat} handleClose={handleChatClose}/> {/* ChatBox 컴포넌트를 추가 */}
+                <ChatBox show={showChat} handleClose={handleChatClose} />
             </div>
         </header>
     );
