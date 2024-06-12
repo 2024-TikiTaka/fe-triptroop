0df143be353d74ff935e91e76f3ff5397bfd85a4
EUNJAE97 <154950170+EUNJAE97@users.noreply.github.com>
2024-06-13 01:13:41 +0900

    #37 일정 상세조회시 지도 표시 (#46)
    * feat: 일정 상세조회시 지도 표시
    
    * Merge branch 'main' into feat/#37-schedule-detail-kakao

--- src/pages/schedule/ScheduleDetail.js
+++ src/pages/schedule/ScheduleDetail.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { callScheduleDetailAPI } from "../../apis/ScheduleAPICalls";
 import ScheduleItem from "../../components/item/ScheduleItem";
+import KakaoMapSelect from "../../components/map/kakaoMapSelect";
 
 function ScheduleDetail() {
     const dispatch = useDispatch();
@@ -11,20 +12,29 @@ function ScheduleDetail() {
     const { schedule } = useSelector(state => state.scheduleReducer);
 
     useEffect(() => {
-        // API 호출 전에 로딩 상태를 true로 설정
-        setLoading(true);
-        dispatch(callScheduleDetailAPI({ scheduleId }))
-            .then(() => {
-                // API 호출이 완료되면 로딩 상태를 false로 설정
-                setLoading(false);
-            });
-    }, [scheduleId]); // scheduleId를 의존성 배열에 추가
+        console.log('useParams로부터 가져온 scheduleId:', scheduleId); // scheduleId 확인
+        const fetchScheduleDetail = async () => {
+            if (scheduleId) {
+                await dispatch(callScheduleDetailAPI({ scheduleId }));
+                setLoading(false); // API 호출이 완료되면 로딩 상태를 false로 설정
+            } else {
+                console.error('scheduleId 유효하지 않습니다.');
+            }
+        };
+
+        fetchScheduleDetail();
+    }, [dispatch, scheduleId]);
+
+    console.log("일정 Details:", schedule);
 
     return (
         <>
             {loading && <div>Loading...</div>} {/* 로딩 중인 경우 로딩 표시 */}
-            {!loading && schedule && ( // 로딩이 완료되고 schedule이 존재하는 경우에만 컴포넌트 렌더링
+            {!loading && schedule && schedule.scheduleItemInfoResponse && ( // 로딩이 완료되고 schedule이 존재하는 경우에만 컴포넌트 렌더링
                 <div className="detail-div">
+                    {schedule.scheduleItemInfoResponse.map(item => (
+                        <KakaoMapSelect key={item.id} place={item} />
+                    ))}
                     <ScheduleItem schedule={schedule} />
                 </div>
             )}
