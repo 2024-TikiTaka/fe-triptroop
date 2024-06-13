983373d95fddc146d71f21d6b50c7a8df67bf067
ddalla0425 <hiwgod92@gmail.com>
2024-06-14 03:19:18 +0900

    feat: 회원 상세 조회 완료

--- src/pages/admin/user/AdminUserDetail.js
+++ src/pages/admin/user/AdminUserDetail.js
@@ -2,7 +2,8 @@ import {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {callAdminUserDetailAPI} from "../../../apis/admin/AdminUserAPICalls";
 import {useParams} from "react-router-dom";
-import DescriptionList from "../../../components/item/admin/DescriptionList";
+import DescriptionList from "../../../components/custom/dl/DescriptionList";
+import {TranslateKeys} from "../../../components/custom/dl/TranslateKeys";
 
 
 const displayOrder = ['userId', 'email', 'nickname', 'role', 'gender', 'name', 'birth', 'phone', 'createdAt', 'provider', 'reportCount', 'godo', 'reviewPoint', 'introduction', 'interestNames', 'mbti', 'contentCount', 'friendCount', 'inquiriesCount', 'status'];
@@ -16,27 +17,25 @@ function AdminUserDetail() {
         dispatch(callAdminUserDetailAPI({userId}));
     }, [dispatch, userId]);
 
-    export const TranslateKeys = (data, context) => {
-        return Object.keys(data).map(key => ({
-            term: TranslateKeys(key, context),
-            description: data[key]
-        }));
-    };
+    const mappedAdmin = TranslateKeys(getInfo, 'admin_user');
 
-    // const items = getInfo && displayOrder.map((key) => ({
-    //     term: defaultKeyTranslations[key] || key,
-    //     description: getInfo[key]
-    // }));
 
-    console.log("items 값 : ", items);
-
-    console.log("admin 값 : ", getInfo);
+    //console.log("items 값 : ", items);
+    console.log("getInfo 값 : ", getInfo);
     console.log("userId 값 : ", userId);
     console.log("dispatch 값 : ", dispatch);
+    console.log("mappedAdmin 값 : ", mappedAdmin);
+
+    // const items = getInfo && displayOrder.map((key) => ({}))
+    const items = getInfo && displayOrder.map((key) => ({
+        term: mappedAdmin[key] || key,
+        description: getInfo[key]
+    }))
+
 
     // 각 행에 들어갈 항목 수를 설정합니다.
     const rowSizes = [2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2]; // 행마다 들어갈 dt, dd 쌍의 수
-    const dtWidth = 100; // dt의 너비
+    const dtWidth = 175; // dt의 너비
 
 
     return (
