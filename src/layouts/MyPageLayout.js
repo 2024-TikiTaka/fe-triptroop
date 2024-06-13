e57480842cbb75688ceac2ecaf068cdf0d8303b3
may54ther <aha9654@gmail.com>
2024-06-13 16:01:14 +0900

    fix: Container 추가

--- src/layouts/MyPageLayout.js
+++ src/layouts/MyPageLayout.js
@@ -5,13 +5,16 @@ import { Outlet } from 'react-router-dom';
 
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
+import { Container } from "react-bootstrap";
 
 function MyPageLayout() {
     return (
         <>
             <Header />
             <main className="main mypage-main">
-                <Outlet />
+                <Container>
+                    <Outlet />
+                </Container>
             </main>
             <Footer />
         </>
