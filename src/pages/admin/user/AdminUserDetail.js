7daa0e14c96000b85d5904d7a02ce8b3850d2711
ddalla0425 <hiwgod92@gmail.com>
2024-06-13 18:06:16 +0900

    feat: 회원 상세 조회 화면에 넣을 컴포넌트 작업중

--- src/pages/admin/user/AdminUserDetail.js
+++ src/pages/admin/user/AdminUserDetail.js
@@ -1,7 +1,40 @@
+import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {callAdminUserDetailAPI} from "../../../apis/admin/AdminUserAPICalls";
+import {useParams} from "react-router-dom";
+import {Col, Container, Row} from "react-bootstrap";
+
 function AdminUserDetail() {
+    const dispatch = useDispatch();
+    const {userId} = useParams();
+    const admin = useSelector(state => state.adminUserReducer.getInfo);
+
+    useEffect(() => {
+        dispatch(callAdminUserDetailAPI({userId}))
+    }, [userId]);
+
 
     return (
         <>
+            <Container>
+                <Row>
+                    <Col as="dl">
+                        <Row>
+                            <Col as="dt" sm={3}>Term 1</Col>
+                            <Col as="dd" sm={9}>Description for term 1.</Col>
+                        </Row>
+                        <Row>
+                            <Col as="dt" sm={3}>Term 2</Col>
+                            <Col as="dd" sm={9}>Description for term 2.</Col>
+                        </Row>
+                        <Row>
+                            <Col as="dt" sm={3}>Term 3</Col>
+                            <Col as="dd" sm={9}>Description for term 3.</Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+
         </>
     );
 }
--- src/pages/admin/user/AdminUserRegist.js
+++ src/pages/admin/user/AdminUserRegist.js
@@ -1,7 +1,7 @@
 function AdminUserRegist() {
 
     return (
-        <>
+        <>Admin USer Regist
         </>
     );
 }
