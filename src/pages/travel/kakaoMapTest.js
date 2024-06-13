001f15daf47d3e9922bcc025f8906255eab3741b
qkrquddjs1 <qkrquddjs9752@naver.com>
2024-06-13 10:10:37 +0900

    chore: key 값 추가

--- src/pages/travel/kakaoMapTest.js
+++ src/pages/travel/kakaoMapTest.js
@@ -6,7 +6,7 @@ const KakaoMapTest = ({ place }) => {
 
     useEffect(() => {
         const script = document.createElement('script');
-        script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_RESTAPI_KAKAO_API_KEY}&libraries=services&autoload=false`;
+        script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=c832e4b58ba1a41ba6ae7d694e9e37e5&libraries=services,clusterer`;
         script.async = true;
         document.head.appendChild(script);
 
