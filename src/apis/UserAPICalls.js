9d5ea538f2622bbff972dd2f1895c3257ae1b8ce
may54ther <aha9654@gmail.com>
2024-06-19 00:40:57 +0900

    feat: user, profile apis

--- src/apis/UserAPICalls.js
+++ src/apis/UserAPICalls.js
@@ -1,41 +1,48 @@
 import { authRequest } from './api';
 import { getUser, success } from "../modules/UserModules";
 import { toast } from "react-toastify";
+import { removeToken } from "../utils/TokenUtils";
 
-
-export const callUserInfoAPI = () => {
-
+export const callCurrentUserAPI = () => {
     return async (dispatch, getState) => {
-        const result = await authRequest.get(`/api/v1/users/me`,);
+        const response = await authRequest.get(`/api/v1/users/me`);
 
-        if (result.status === 200) {
-            dispatch(getUser(result.data.result));
+        if (response.status === 200) {
+            dispatch(getUser(response.data.result));
         }
     };
 };
-
-export const callChangePasswordAPI = () => {
+export const callChangePasswordAPI = (currentPassword, newPassword) => {
     return async (dispatch, getState) => {
-        return await authRequest.post(`/api/v1/users/me/password`)
-                                .then(response => {
-                                    dispatch(getUser(response.data.result));
-                                })
-                                .catch(error => {
-                                    console.log(error);
-                                });
+        return await authRequest.post(`/api/v1/users/me/password`, JSON.stringify({ currentPassword, newPassword }))
+                                .then(response => response.data)
+                                .catch(error => error);
     };
 };
+
+
+export const callCheckPasswordAPI = (password) => {
+    return async (dispatch, getState) => {
+        return await authRequest.post(`/api/v1/users/me/password`, JSON.stringify({ password }))
+                                .then(response => response.data)
+                                .catch(error => error);
+    };
+};
+
 export const callWithdrawalAPI = ({ email, password }) => {
     return async (dispatch, getState) => {
-        const result = await authRequest.delete(
-            `/api/v1/profiles/me`,
-            new URLSearchParams({ email: email, password: password })
-        ).then(response => {
-            dispatch(success());
-            toast.info("회원 탈퇴가 완료되었습니다.");
-        }).catch(error => {
-            console.log(error);
-        });
+        const response = await authRequest.delete(`/api/v1/profiles/me`, new URLSearchParams({ email, password }))
+                                          .then(response => {
+                                              if (response.status === 204) {
+                                                  dispatch(success());
+                                                  removeToken();
+                                                  toast.info('회원 탈퇴가 완료되었습니다.');
+                                              }
+                                          })
+                                          .catch(error => {
+                                              console.log(error);
+                                          });
     };
 };
+
 
