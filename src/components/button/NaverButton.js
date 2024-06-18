e33327bd85b8e7c120d92750bdda977f18b5bf44
may54ther <aha9654@gmail.com>
2024-06-19 00:53:22 +0900

    feat: email, kakao, naver 버튼 컴포넌트

--- src/components/button/NaverButton.js
+++ src/components/button/NaverButton.js
@@ -3,19 +3,27 @@ import { Button } from "react-bootstrap";
 import { Naver } from "../common/Icons";
 
 const NaverButton = () => {
-  return (
-    <>
-      <Button
-        variant="light"
-        size={"lg"}
-        style={{ background: "#03c75a" }}
-        className="fs-6 mb-0"
-      >
-        <Naver width={"18"} fill={"#fff"} />
-        <span className="ms-2 text-white">네이버로 시작하기</span>
-      </Button>
-    </>
-  );
+    // const REST_API_KEY = `${process.env.REACT_APP_KAKAO_SECRET}`;
+    // const REDIRECT_URI = `${process.env.REACT_APP_KAKAO_URL}`;
+    // const NAVER_DEFAULT_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    //
+    // const handleClick = () => {
+    //     window.location.href = NAVER_DEFAULT_URL;
+    // };
+    return (
+        <>
+            <Button
+                variant="light"
+                size={"lg"}
+                style={{ background: "#03c75a" }}
+                className="fs-6 mb-0"
+                // onClick={handleClick}
+            >
+                <Naver width={"18"} fill={"#fff"} />
+                <span className="ms-2 text-white">네이버로 시작하기</span>
+            </Button>
+        </>
+    );
 };
 
 export default NaverButton;
